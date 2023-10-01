3ea768b8e913006cd5d0528fb0bf25c6352293c9
georemo <george.oremo@gmail.com>
2023-10-01 10:56:27 +0300

    -

--- src/CdApi/sys/cd-push/services/sio.service.ts
+++ src/CdApi/sys/cd-push/services/sio.service.ts
@@ -17,15 +17,15 @@ dotenv.config();
  * overload the default console.log function
  * for debugging
  */
-const fs = require('fs');
-const util = require('util');
-const log_file = fs.createWriteStream(__dirname + '/debug.log', { flags: 'w' });
-const log_stdout = process.stdout;
+// const fs = require('fs');
+// const util = require('util');
+// const log_file = fs.createWriteStream(__dirname + '/debug.log', { flags: 'w' });
+// const log_stdout = process.stdout;
 
-console.log = function (d) { //
-    log_file.write(util.format(d) + '\n');
-    log_stdout.write(util.format(d) + '\n');
-};
+// console.log = function (d) { //
+//     log_file.write(util.format(d) + '\n');
+//     log_stdout.write(util.format(d) + '\n');
+// };
 ////////////////////////////////////
 
 // const io = new Server();
