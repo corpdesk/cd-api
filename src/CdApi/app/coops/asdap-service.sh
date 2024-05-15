bedea2539a95f6a526190b23a3feac1c46f26632
“georemo” <george.oremo@gmail.com>
2024-05-15 09:59:12 +0300

    -

--- src/CdApi/app/coops/asdap-service.sh
+++ src/CdApi/app/coops/asdap-service.sh
@@ -5,12 +5,15 @@ npm install npm i ts-node typescript
 chmod +x /home/devops/cd-api/src/app.ts
 chmod +x /home/devops/cd-sio/src/app.ts
 sudo cp /home/devops/cd-api/src/CdApi/app/coops/asdap.service /etc/systemd/system/
-sudo cp /home/devops/cd-api/src/CdApi/app/coops/asdap.service /etc/systemd/system/
+sudo cp /home/devops/cd-api/src/CdApi/app/coops/asdap-sio.service /etc/systemd/system/
+bash -c 'sudo ln -s /usr/bin/nodejs /usr/bin/node'
 sudo systemctl daemon-reload
 sudo systemctl start asdap
 sudo systemctl start asdap-sio
 sudo systemctl enable asdap
 sudo systemctl enable asdap-sio
+# sudo journalctl -u asdap
+# sudo journalctl -u asdap-sio
 
 
 
