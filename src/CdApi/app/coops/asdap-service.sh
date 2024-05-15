4d58eb45f81e311b49f25b8e53e8a57d6bdfc7c4
“georemo” <george.oremo@gmail.com>
2024-05-15 12:30:53 +0300

    -

--- src/CdApi/app/coops/asdap-service.sh
+++ src/CdApi/app/coops/asdap-service.sh
@@ -7,7 +7,8 @@ chmod +x /home/devops/cd-sio/src/app.ts
 sudo cp /home/devops/cd-api/src/CdApi/app/coops/asdap.service /etc/systemd/system/
 sudo cp /home/devops/cd-api/src/CdApi/app/coops/asdap-sio.service /etc/systemd/system/
 sudo rm -f /usr/bin/node
-bash -c 'sudo ln -s /usr/bin/nodejs /usr/bin/node'
+bash -c 'sudo ln -s "$(which node)" /usr/bin/node'
+# bash -c 'sudo ln -s /usr/bin/nodejs /usr/bin/node'
 sudo systemctl daemon-reload
 sudo systemctl start asdap
 sudo systemctl start asdap-sio
@@ -15,6 +16,7 @@ sudo systemctl enable asdap
 sudo systemctl enable asdap-sio
 # sudo journalctl -u asdap
 # sudo journalctl -u asdap-sio
+# sudo journalctl --unit=masdap -n 30 --no-pager
 
 
 
