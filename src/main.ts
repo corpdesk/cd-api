685e51f1ce1e0dab4276c4a2cd59e99aaff8503e
“georemo” <george.oremo@gmail.com>
2024-05-20 23:02:58 +0300

    -

--- src/main.ts
+++ src/main.ts
@@ -105,6 +105,17 @@ export class Main {
             });
         ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+        app.all('*', function (req, res, next) {
+            res.header('Access-Control-Allow-Origin', 'URLs to trust of allow');
+            res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+            res.header('Access-Control-Allow-Headers', 'Content-Type');
+            if ('OPTIONS' == req.method) {
+                res.sendStatus(200);
+            } else {
+                next();
+            }
+        });
+
 
         // set api entry point
         app.post('/api', async (req: any, res: any) => {
