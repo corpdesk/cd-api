67294a26b9550e979bff3c847a606649b062f98b
“georemo” <george.oremo@gmail.com>
2024-05-22 16:25:13 +0300

    -

--- sdk/cd-sio-daemon.sh
+++ sdk/cd-sio-daemon.sh
@@ -1,9 +1,10 @@
+# error: /usr/bin/env: ‘node’: No such file or directory when "sudo ln -s /usr/bin/nodejs /usr/bin/node" is used
 # npm install -g npm i ts-node typescript
 # sudo rm -f /usr/bin/node
 # sudo ln -s /usr/bin/nodejs /usr/bin/node
-chmod +x /home/devops/cd-sio/src/app.ts
-sudo cp /home/devops/cd-sio/sdk/cd-sio.service /etc/systemd/system/
-sudo systemctl daemon-reload
-sudo systemctl start cd-sio
-sudo systemctl enable cd-sio
-journalctl -fu cd-sio
+# sudo chmod +x /home/devops/cd-sio/src/app.ts
+# sudo cp /home/devops/cd-sio/sdk/cd-sio.service /etc/systemd/system/
+# sudo systemctl daemon-reload
+# sudo systemctl start cd-sio
+# sudo systemctl enable cd-sio
+# journalctl -fu cd-sio
