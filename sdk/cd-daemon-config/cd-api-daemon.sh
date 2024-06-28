39c4a8fe603666ecd8f346d3d91c2e460832d08b
“georemo” <george.oremo@gmail.com>
2024-06-28 11:39:36 +0300

    -

--- sdk/cd-daemon-config/cd-api-daemon.sh
+++ sdk/cd-daemon-config/cd-api-daemon.sh
@@ -2,7 +2,7 @@
 # sudo rm -f /usr/bin/node
 # sudo ln -s /usr/bin/nodejs /usr/bin/node
 # chmod +x /home/devops/cd-api/src/app.ts
-# sudo cp /home/devops/cd-api/sdk/cd-api.service /etc/systemd/system/
+# sudo cp /home/devops/cd-api/sdk/cd-daemon-config/cd-api.service /etc/systemd/system/
 # sudo systemctl daemon-reload
 # sudo systemctl start cd-api
 # sudo systemctl enable cd-api
--- sdk/cd-daemon-config/cd-sio-daemon.sh
+++ sdk/cd-daemon-config/cd-sio-daemon.sh
@@ -3,7 +3,7 @@
 # sudo rm -f /usr/bin/node
 # sudo ln -s /usr/bin/nodejs /usr/bin/node
 # sudo chmod +x /home/devops/cd-sio/src/app.ts
-# sudo cp /home/devops/cd-sio/sdk/cd-sio.service /etc/systemd/system/
+# sudo cp /home/devops/cd-sio/sdk/cd-daemon-config/cd-sio.service /etc/systemd/system/
 # sudo systemctl daemon-reload
 # sudo systemctl start cd-sio
 # sudo systemctl enable cd-sio
