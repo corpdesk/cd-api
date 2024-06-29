bd2cbc190393000feb1beb599958c70c92eeb016
“georemo” <george.oremo@gmail.com>
2024-06-30 00:00:25 +0300

    -

--- src/main-api.ts
+++ src/main-api.ts
@@ -194,7 +194,7 @@ export class Main {
             console.log("app.post/01")
             res.setHeader('Content-Type', 'application/json');
             ////////////////////
-            // res.setHeader("Access-Control-Allow-Origin", "*");
+            res.setHeader("Access-Control-Allow-Origin", "*");
             res.setHeader("Access-Control-Allow-Credentials", "true");
             res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
             res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
