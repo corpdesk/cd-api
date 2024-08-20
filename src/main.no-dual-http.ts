6fed6516947c416f6bd305e76c64ef7bf2c24bb6
“georemo” <george.oremo@gmail.com>
2024-08-20 09:44:32 +0300

    -

---
+++ src/main.no-dual-http.ts
@@ -0,0 +1,209 @@
+import config from './config';
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import 'reflect-metadata';
+
+// database imports
+import { MysqlDataSource as ds } from './CdApi/sys/base/data-source';
+
+// corpdesk engine imports
+import { CdInit } from './CdApi/init';
+
+// push server imports
+import { createClient, RedisClientOptions } from 'redis';
+import { createAdapter } from '@socket.io/redis-adapter';
+import { createServer as createHttpServer } from 'http';
+import { createServer as createHttpsServer } from 'https';
+import fs from 'fs';
+import path from 'path';
+import * as WebSocket from 'ws';
+import { Server, Socket } from 'socket.io';
+import Redis from "ioredis";
+import { SioService } from './CdApi/sys/cd-push/services/sio.service';
+import { Logging } from './CdApi/sys/base/winston.log';
+import { WebsocketService } from './CdApi/sys/cd-push/services/websocket.service';
+import pusher from './CdApi/sys/cd-push/services/pusher';
+
+export class Main {
+    logger: Logging;
+    allowedOrigins = ["https://asdap.net"];
+
+    constructor() {
+        this.logger = new Logging();
+    }
+
+    async run() {
+        this.logger.logInfo('Main::run()/01');
+
+        const app: Application = express();
+
+        // Set up SSL credentials
+        const privateKey = fs.readFileSync(config.keyPath, 'utf8');
+        const certificate = fs.readFileSync(config.certPath, 'utf8');
+
+        let certAuth = '';
+        if (config.caPath.length > 0) {
+            certAuth = fs.readFileSync(config.caPath, 'utf8');
+        } else {
+            certAuth = null;
+        }
+
+        const credentials = {
+            key: privateKey,
+            cert: certificate,
+            // ca: certAuth
+        };
+
+        // CORS configuration
+        const corsOptions = {
+            origin: 'https://asdap.net',
+            // origin: 'http://localhost',
+            methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+            allowedHeaders: ['Content-Type', 'Authorization'],
+            credentials: true
+        };
+        app.use(cors(corsOptions));
+        app.use(express.json()); // For parsing application/json
+        app.options('*', cors(corsOptions)); // Enable pre-flight across-the-board
+
+        // Serve .well-known directory for Let's Encrypt validation
+        // app.get('/.well-known/acme-challenge', express.static(path.join(__dirname, '.well-known/acme-challenge')));
+        // Serve .well-known/acme-challenge directory for Let's Encrypt validation
+        // app.use('/.well-known/acme-challenge', express.static(path.join(__dirname, '.well-known/acme-challenge')));
+        
+        // To test: curl http://localhost:8080/.well-known/acme-challenge/test-file -v
+        app.use(config.http.webroot, express.static(path.join(__dirname, config.http.webroot)));
+
+        // // Define a route to serve the HTML file
+        // app.get('/well-known/acme-challenge', (req, res) => {
+        //     // Send the HTML file as the response
+        //     res.sendFile(path.join(__dirname, '.well-known/acme-challenge/test-file.html'));
+        // });
+
+        // Create HTTP server
+        const httpServer = createHttpServer(app);
+
+        // Create HTTPS server
+        const httpsServer = createHttpsServer(credentials, app);
+
+        /**
+         * Handle socket.io connections with SSL (for production)
+         */
+        if (config.pushService.sio.enabled) {
+            this.logger.logInfo('Main::run()/02');
+
+            const io = new Server(httpsServer, {
+                cors: {
+                    origin: 'https://asdap.net',
+                    methods: ['GET', 'POST'],
+                    credentials: true
+                }
+            });
+
+            this.logger.logInfo('Main::run()/03');
+            this.logger.logInfo('Main::run()/config.push.mode:', { mode: config.push.mode });
+
+            let pubClient;
+            let subClient;
+            switch (config.push.mode) {
+                case "PUSH_BASIC":
+                    this.logger.logInfo('Main::run()/031');
+                    pubClient = createClient({ host: config.push.redisHost, port: config.push.redisPort, legacyMode: true } as RedisClientOptions);
+                    subClient = pubClient.duplicate();
+                    break;
+                case "PUSH_CLUSTER":
+                    this.logger.logInfo('Main::run()/032');
+                    pubClient = new Redis.Cluster(config.push.startupNodes);
+                    subClient = pubClient.duplicate();
+                    break;
+                case "PUSH_SENTINEL":
+                    this.logger.logInfo('Main::run()/033');
+                    pubClient = new Redis(config.push.sentinalOptions);
+                    subClient = pubClient.duplicate();
+                    break;
+                default:
+                    this.logger.logInfo('Main::run()/034');
+                    pubClient = createClient({ host: config.push.redisHost, port: config.push.redisPort } as RedisClientOptions);
+                    subClient = pubClient.duplicate();
+                    break;
+            }
+
+            Promise.all([pubClient, subClient])
+                .then(() => {
+                    this.logger.logInfo('Main::run()/035');
+                    const svSio = new SioService();
+                    svSio.run(io, pubClient, subClient);
+                });
+        }
+
+        /**
+         * Set up HTTP routing for API and redirect to HTTPS if needed
+         */
+        if (config.apiRoute === "/api" && config.secure === "false") {
+            console.log("main/04");
+            app.post(config.apiRoute, async (req: Request, res: Response) => {
+                res.setHeader('Content-Type', 'application/json');
+                res.setHeader("Access-Control-Allow-Credentials", "true");
+                res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+                res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+                CdInit(req, res, ds);
+            });
+        }
+
+        // Push service endpoints
+        if (config.pushService.pusher.enabled) {
+            app.post('/notify', (req: Request, res: Response) => {
+                res.setHeader("Access-Control-Allow-Credentials", "true");
+                res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+                res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+                const { message, channel, event } = req.body;
+                pusher.trigger(channel, event, { message: "hello from server on '/notify'" })
+                    .then(() => res.status(200).send("Notification sent from '/notify'"))
+                    .catch((err: Error) => res.status(500).send(`Error sending notification: ${err.message}`));
+            });
+
+            app.post('/notify-user', (req: Request, res: Response) => {
+                res.setHeader("Access-Control-Allow-Credentials", "true");
+                res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+                res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+                const { message, userId } = req.body;
+                const channel = `private-user-${userId}`;
+
+                pusher.trigger(channel, 'user-event', { message: "hello from server on '/notify-user'" })
+                    .then(() => res.status(200).send("Notification sent from '/notify'"))
+                    .catch((err: Error) => res.status(500).send(`Error sending notification: ${err.message}`));
+            });
+
+            app.post('/pusher/auth', (req: Request, res: Response) => {
+                res.setHeader("Access-Control-Allow-Credentials", "true");
+                res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+                res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+                const socketId = req.body.socket_id;
+                const channel = req.body.channel_name;
+                const auth = pusher.authenticate(socketId, channel);
+                res.send(auth);
+            });
+        }
+
+        if (config.http.enabled) {
+            // Start HTTP server (for Let's Encrypt and optional redirect to HTTPS)
+            httpServer.listen(config.http.port, () => {
+                this.logger.logInfo(`HTTP server listening on port ${config.http.port}`);
+            }).on('error', (e) => {
+                this.logger.logError(`HTTP server: listen()/Error: ${e}`);
+            });
+        }
+
+
+        // Start HTTPS server
+        httpsServer.listen(config.apiPort, () => {
+            this.logger.logInfo(`HTTPS server is listening on port ${config.apiPort}`);
+        }).on('error', (e) => {
+            this.logger.logError(`HTTPS server: listen()/Error: ${e}`);
+        });
+    }
+
+    originIsAllowed(origin: string) {
+        return this.allowedOrigins.includes(origin);
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
+// basic imports
+
 import config from './config';
+// import express from 'express';
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import 'reflect-metadata';
@@ -12,11 +15,14 @@ import { CdInit } from './CdApi/init';
 // push server imports
 import { createClient, RedisClientOptions } from 'redis';
 import { createAdapter } from '@socket.io/redis-adapter';
+import { createServer } from 'http';
 import { createServer as createHttpServer } from 'http';
+import https from 'https';
 import { createServer as createHttpsServer } from 'https';
 import fs from 'fs';
 import path from 'path';
 import * as WebSocket from 'ws';
+import { server as WebSocketServer } from 'websocket';
 import { Server, Socket } from 'socket.io';
 import Redis from "ioredis";
 import { SioService } from './CdApi/sys/cd-push/services/sio.service';
@@ -24,28 +30,35 @@ import { Logging } from './CdApi/sys/base/winston.log';
 import { WebsocketService } from './CdApi/sys/cd-push/services/websocket.service';
 import pusher from './CdApi/sys/cd-push/services/pusher';
 
+
+
 export class Main {
     logger: Logging;
     allowedOrigins = ["https://asdap.net"];
-
     constructor() {
         this.logger = new Logging();
     }
-
     async run() {
-        this.logger.logInfo('Main::run()/01');
-
+        this.logger.logInfo('Main::run()/01')
+        // this.logger.logInfo('Main::run()/config.keyPath', {keyPath: config.keyPath} )
+        // this.logger.logInfo('Main::run()/config.certPath', {certPath: config.certPath} )
+        // this.logger.logInfo('Main::run()/config.caPath', {caPath: config.caPath} )
+        // basic settings
         const app: Application = express();
 
-        // Set up SSL credentials
+        // Serve .well-known directory for Let's Encrypt validation
+        // app.use('/.well-known/acme-challenge', express.static(path.join(__dirname, '.well-known/acme-challenge')));
+
         const privateKey = fs.readFileSync(config.keyPath, 'utf8');
         const certificate = fs.readFileSync(config.certPath, 'utf8');
 
+
         let certAuth = '';
+        // just in case certificate authority is not provided
         if (config.caPath.length > 0) {
             certAuth = fs.readFileSync(config.caPath, 'utf8');
         } else {
-            certAuth = null;
+            certAuth = null
         }
 
         const credentials = {
@@ -54,44 +67,55 @@ export class Main {
             // ca: certAuth
         };
 
-        // CORS configuration
+
+        const options = config.Cors.options;
+
+        ////////////////////////////////////////////////////////////////////////////////
         const corsOptions = {
-            origin: 'https://asdap.net',
-            // origin: 'http://localhost',
+            // origin: 'https://asdap.net', // Replace with your client URL
+            origin: 'http://localhost',
             methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
             allowedHeaders: ['Content-Type', 'Authorization'],
             credentials: true
         };
+        ////////////////////////////////////////////////////////////////////////////////
+
+        let httpsServer = null;
+        let corsOpts = null;
+
+        //////////////////////////////////////////////////////////////////////////////
         app.use(cors(corsOptions));
         app.use(express.json()); // For parsing application/json
         app.options('*', cors(corsOptions)); // Enable pre-flight across-the-board
+        //////////////////////////////////////////////////////////////////////////////
 
         // Serve .well-known directory for Let's Encrypt validation
-        // app.get('/.well-known/acme-challenge', express.static(path.join(__dirname, '.well-known/acme-challenge')));
-        // Serve .well-known/acme-challenge directory for Let's Encrypt validation
-        // app.use('/.well-known/acme-challenge', express.static(path.join(__dirname, '.well-known/acme-challenge')));
-        
         // To test: curl http://localhost:8080/.well-known/acme-challenge/test-file -v
         app.use(config.http.webroot, express.static(path.join(__dirname, config.http.webroot)));
-
-        // // Define a route to serve the HTML file
-        // app.get('/well-known/acme-challenge', (req, res) => {
-        //     // Send the HTML file as the response
-        //     res.sendFile(path.join(__dirname, '.well-known/acme-challenge/test-file.html'));
-        // });
 
         // Create HTTP server
         const httpServer = createHttpServer(app);
 
         // Create HTTPS server
-        const httpsServer = createHttpsServer(credentials, app);
+        httpsServer = https.createServer(credentials, app);
+        corsOpts = {
+            cors: {
+                options: config.Cors.options.allowedHeaders,
+                origin: config.Cors.options.origin
+            }
+        }
+
 
         /**
-         * Handle socket.io connections with SSL (for production)
+         * When run on sio mode in production,
+         * use SSL
+         * use cors
          */
         if (config.pushService.sio.enabled) {
-            this.logger.logInfo('Main::run()/02');
+            this.logger.logInfo('Main::run()/02')
 
+            // const io = new Server(httpsServer, corsOpts);
+            /////////////////////////////////////////////////////
             const io = new Server(httpsServer, {
                 cors: {
                     origin: 'https://asdap.net',
@@ -99,30 +123,30 @@ export class Main {
                     credentials: true
                 }
             });
+            /////////////////////////////////////////////////////
 
-            this.logger.logInfo('Main::run()/03');
-            this.logger.logInfo('Main::run()/config.push.mode:', { mode: config.push.mode });
-
+            this.logger.logInfo('Main::run()/03')
+            this.logger.logInfo('Main::run()/config.push.mode:', { mode: config.push.mode })
             let pubClient;
             let subClient;
             switch (config.push.mode) {
                 case "PUSH_BASIC":
-                    this.logger.logInfo('Main::run()/031');
+                    this.logger.logInfo('Main::run()/031')
                     pubClient = createClient({ host: config.push.redisHost, port: config.push.redisPort, legacyMode: true } as RedisClientOptions);
                     subClient = pubClient.duplicate();
                     break;
                 case "PUSH_CLUSTER":
-                    this.logger.logInfo('Main::run()/032');
+                    this.logger.logInfo('Main::run()/032')
                     pubClient = new Redis.Cluster(config.push.startupNodes);
                     subClient = pubClient.duplicate();
                     break;
                 case "PUSH_SENTINEL":
-                    this.logger.logInfo('Main::run()/033');
+                    this.logger.logInfo('Main::run()/033')
                     pubClient = new Redis(config.push.sentinalOptions);
                     subClient = pubClient.duplicate();
                     break;
                 default:
-                    this.logger.logInfo('Main::run()/034');
+                    this.logger.logInfo('Main::run()/034')
                     pubClient = createClient({ host: config.push.redisHost, port: config.push.redisPort } as RedisClientOptions);
                     subClient = pubClient.duplicate();
                     break;
@@ -130,33 +154,75 @@ export class Main {
 
             Promise.all([pubClient, subClient])
                 .then(() => {
-                    this.logger.logInfo('Main::run()/035');
+                    this.logger.logInfo('Main::run()/035')
                     const svSio = new SioService();
-                    svSio.run(io, pubClient, subClient);
+                    svSio.run(io, pubClient, subClient)
                 });
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         }
 
         /**
-         * Set up HTTP routing for API and redirect to HTTPS if needed
+         * When run on app mode in production,
+         * use without SSL...but behind nginx proxy server fitted with SSL
+         * do not use cors...but set it at nginx
          */
         if (config.apiRoute === "/api" && config.secure === "false") {
-            console.log("main/04");
-            app.post(config.apiRoute, async (req: Request, res: Response) => {
-                res.setHeader('Content-Type', 'application/json');
-                res.setHeader("Access-Control-Allow-Credentials", "true");
-                res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-                res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-                CdInit(req, res, ds);
-            });
+            console.log("main/04")
+            httpsServer = createServer(app);
+            corsOpts = {
+                cors: {
+                    options: config.Cors.options.allowedHeaders,
+                    origin: null
+                }
+            }
         }
 
-        // Push service endpoints
+        // app.all('*', function (req, res, next) {
+        //     // res.header('Access-Control-Allow-Origin', 'URLs to trust of allow');
+        //     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        //     res.header('Access-Control-Allow-Headers', 'Content-Type');
+        //     if ('OPTIONS' == req.method) {
+        //         res.sendStatus(200);
+        //     } else {
+        //         next();
+        //     }
+        // });
+
+        app.post('/sio/p-reg/', async (req: any, res: any) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.setHeader("Access-Control-Allow-Credentials", "true");
+            res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+            res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+            CdInit(req, res);
+        });
+
+        // Handle POST requests
+        // app.post('/sio/p-reg', (req: Request, res: Response) => {
+        //     const { name, email } = req.body;
+        //     console.log(`Received registration: Name: ${name}, Email: ${email}`);
+        //     res.send({ message: 'Registration successful' });
+        // });
+
+
+        // set api entry point
+        app.post(config.apiRoute, async (req: any, res: any) => {
+            console.log("app.post/01")
+            res.setHeader('Content-Type', 'application/json');
+            ////////////////////
+            // res.setHeader("Access-Control-Allow-Origin", "*");
+            res.setHeader("Access-Control-Allow-Credentials", "true");
+            res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+            res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+            CdInit(req, res, ds);
+        });
+
         if (config.pushService.pusher.enabled) {
             app.post('/notify', (req: Request, res: Response) => {
                 res.setHeader("Access-Control-Allow-Credentials", "true");
                 res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
                 res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
                 const { message, channel, event } = req.body;
+                // this.logger.logInfo("message:", message)
                 pusher.trigger(channel, event, { message: "hello from server on '/notify'" })
                     .then(() => res.status(200).send("Notification sent from '/notify'"))
                     .catch((err: Error) => res.status(500).send(`Error sending notification: ${err.message}`));
@@ -185,25 +251,85 @@ export class Main {
             });
         }
 
-        if (config.http.enabled) {
-            // Start HTTP server (for Let's Encrypt and optional redirect to HTTPS)
-            httpServer.listen(config.http.port, () => {
-                this.logger.logInfo(`HTTP server listening on port ${config.http.port}`);
-            }).on('error', (e) => {
-                this.logger.logError(`HTTP server: listen()/Error: ${e}`);
+        if (config.pushService.wss.enabled) {
+
+            console.log("main/05")
+            const expressServer = app.listen(config.wssPort, () => {
+                console.log(`server is listening on ${config.wssPort}`);
+            })
+                .on('error', (e) => {
+                    console.log(`Error:${e}`);
+                });
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////
+            // Define the WebSocket server. Here, the server mounts to the `/ws`
+            // route of the Express JS server.
+            const wss = new WebSocket.Server({
+                server: expressServer,
+                path: '/ws'
             });
+            /**
+             * run the websocket
+             */
+            const cdWs = new WebsocketService();
+            cdWs.run(wss);
+
+
+            // ///////////////////////////////////////////////////
+            // const server = createHttpsServer(credentials, app);
+            // const wsServer = new WebSocketServer({
+            //     httpsServer: server,
+            //     autoAcceptConnections: false
+            // });
+
+
+            // wsServer.on('request', function (request) {
+            //     if (!this.originIsAllowed(request.origin)) {
+            //         request.reject();
+            //         console.log('Connection from origin ' + request.origin + ' rejected.');
+            //         return;
+            //     }
+
+            //     const connection = request.accept(null, request.origin);
+            //     console.log('Connection accepted.');
+
+            //     connection.on('message', function (message) {
+            //         if (message.type === 'utf8') {
+            //             console.log('Received Message: ' + message.utf8Data);
+            //             connection.sendUTF('Hello Client');
+            //         }
+            //     });
+
+            //     connection.on('close', function (reasonCode, description) {
+            //         console.log('Peer ' + connection.remoteAddress + ' disconnected.');
+            //     });
+            // });
+
+        } else {
+
+            if (config.http.enabled) {
+                // Start HTTP server (for Let's Encrypt and optional redirect to HTTPS)
+                httpServer.listen(config.http.port, () => {
+                    this.logger.logInfo(`HTTP server listening on port ${config.http.port}`);
+                }).on('error', (e) => {
+                    this.logger.logError(`HTTP server: listen()/Error: ${e}`);
+                });
+            }
+
+            // start server
+            httpsServer.listen(config.apiPort, () => {
+                // console.log(`cd-api server is listening on ${config.apiPort}`);
+                this.logger.logInfo(`cd-api server is listening on:`, { port: `${config.apiPort}` })
+            })
+                .on('error', (e) => {
+                    this.logger.logError(`cd-api server: listen()/Error:${e}`)
+                });
         }
 
-
-        // Start HTTPS server
-        httpsServer.listen(config.apiPort, () => {
-            this.logger.logInfo(`HTTPS server is listening on port ${config.apiPort}`);
-        }).on('error', (e) => {
-            this.logger.logError(`HTTPS server: listen()/Error: ${e}`);
-        });
     }
 
     originIsAllowed(origin: string) {
         return this.allowedOrigins.includes(origin);
     }
+
 }
+
